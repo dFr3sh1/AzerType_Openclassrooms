b8d71f7e49c8649db81d312e444bb6851d127eb2
DFr3sh <dfr3sh1@protonmail.com>
2024-02-16 17:20:27 +0100

    Cleaned main.js and added a validation with the enter key in the playGame function

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 //Function to display the score    
 function displayScore(score, wordsNumber) {
-    //Create new variables for displaying the socre in the html
+    //Create new variables for displaying the score in the html
     let scoreSpan = document.querySelector(".zoneScore span")
 
     let newDisplayScore = `${score} / ${wordsNumber}`
@@ -24,23 +24,36 @@ function playGame() {
     let inputEcriture = document.getElementById('inputEcriture')
     let i = 0
 
+//Displaies the table listeDeMots
+
     displayProposition(listeDeMots[i])
     validationBtn.addEventListener('click', () => {
         console.log(inputEcriture.value)
+
+        //If the value is the same than inputEcriture the score increases
         if (inputEcriture.value === listeDeMots[i]) {
             score++
         }
         i++
         displayScore(score, i)
         inputEcriture.value = ''
+
+        //If the value of listeDeMots is undefined the gameis over
         if (listeDeMots[i] === undefined) {
             displayProposition("Le jeu est fini")
             validationBtn.disabled = true
             
         } else {
+            //To clean the fill of proposition
             displayProposition(listeDeMots[i])
         }
 
+    });
+//To validate the word pressing the Enter key
+    inputEcriture.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            validationBtn.click()
+        }
     })
 
     displayScore(score, i)
