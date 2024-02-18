d575280f6b88887bbe9ec8428721e205a1095884
DFr3sh <dfr3sh1@protonmail.com>
2024-02-18 14:03:28 +0100

    Perform the playGame function creating a new function tomanage the form

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,9 @@
-//Function to display the score    
+//Function to display the score
+/**
+ * 
+ * @param {number} score 
+ * @param {number} wordsNumber 
+ */    
 function displayScore(score, wordsNumber) {
     //Create new variables for displaying the score in the html
     let scoreSpan = document.querySelector(".zoneScore span")
@@ -11,30 +16,63 @@ function displayScore(score, wordsNumber) {
     //Display the score
     console.log('Votre score est ' + score + ' sur ' + wordsNumber)
 }
-
+/**
+ * 
+ * @param {string} proposition 
+ */
 function displayProposition(proposition) {
     let zoneProposition = document.querySelector('.zoneProposition')
     zoneProposition.innerText = proposition
 };
-
+/**
+ * 
+ * @param {string} name 
+ * @param {email} email 
+ * @param {number} score 
+ */
 function displayEmail(name, email, score) {
     let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${name} et je viens de réaliser le score ${score} sur le site d'Azertype !`
     location.href = mailto
 }
-
+/**
+ * 
+ * @param {string} name 
+ * @returns {boolean}
+ */
 function verifyName(name){
     if (name.length >= 2) {
         return true
     }
     return false
 }
-
+/**
+ * 
+ * @param {email} email 
+ * @returns {boolean}
+ */
 function verifyEmail(email) {
     let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
     if (emailRegExp.test(email)) {
         return true
     }
     return false
+}
+/**
+ * 
+ * @param {number} tagScore 
+ */
+function formManager(tagScore) {
+    let tagName = document.getElementById("nom")
+    let name = tagName.value
+    
+    let tagEmail = document.getElementById("email")
+    let email = tagEmail.value
+    
+    if (verifyName(name) && verifyEmail(email)) {
+        displayEmail(name, email, tagScore)
+    } else {
+        console.log("Erreur")
+    }
 }
 
 //Main function to launch the game
@@ -97,19 +135,8 @@ for (let index = 0; index< choice.length; index++) {
     let form = document.querySelector("form")
     form.addEventListener("submit", (event) => {
         event.preventDefault()
-        
-        let tagName = document.getElementById("nom")
-        let name = tagName.value
-        
-        let tagEmail = document.getElementById("email")
-        let email = tagEmail.value
-        
-        if (verifyName(name) && verifyEmail(email)) {
-            let tagScore = `${score} / ${i}`
-            displayEmail(name, email, tagScore)
-        } else {
-            console.log("Erreur")
-        }
+        let tagScore = `${score} / ${i}`
+        formManager(tagScore)
     })
 
     displayScore(score, i)
